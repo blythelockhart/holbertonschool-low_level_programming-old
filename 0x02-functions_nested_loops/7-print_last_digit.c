de509bd0c524faaab83f4bf11f246793695b526b
blythelockhart <blythe.lockhart@att.net>
2023-02-05 15:45:48 -0800

    0x02-functions_nested_loops commit 8

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * print_last_digit - Prints the last digit of a number
+ * @n: The number to be treated
+ * Return: Value of the last digit of a number
  */
 int print_last_digit(int n)
 {
@@ -14,5 +14,5 @@ if (last < 0)
 last = last * -1;
 }
 _putchar(last + '0');
-return (0);
+return (last);
 }
