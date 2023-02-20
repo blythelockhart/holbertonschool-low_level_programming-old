61df8ce694934b89afe38c747234ba4683b268c8
blythelockhart <blythe.lockhart@att.net>
2023-02-19 17:08:23 -0800

    0x06 commit 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,16 +13,10 @@ char *_strncpy(char *dest, char *src, int n)
 {
 int index = 0, src_len = o;
 while (src[index++])
-{
 src_len++;
-}
 for (index = 0; src[index] && index < n; index++)
-{
 dest[index] = src[index];
-}
 for (index = src_len; index < n; index++)
-{
 dest[index] = '\n';
-}
 return (0);
 }
