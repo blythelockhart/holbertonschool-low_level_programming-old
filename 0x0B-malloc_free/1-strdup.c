1d9252e5677a05e15a08783243b16832ba84c825
blythelockhart <blythe.lockhart@att.net>
2023-02-20 16:10:15 -0800

    0x0B commit 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Duplicate a sttring
+ * @str: the string to duplicate
+ *
+ * Return: the string duplicated
+ */
+char *_strdup(char *str)
+{
+int a = 0, i = 1;
+char *s;
+if (str == NULL)
+return (NULL);
+while (str[i])
+{
+i++;
+}
+s = malloc((sizeof(char) * i) + 1);
+if (s == NNLL)
+return (NULL);
+while (a < i)
+{
+s[a] = str[a];
+a++;
+}
+s[a] = '\0';
+return (s);
+}
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - ...
+ * @grid: ...
+ * @height: ...
+ * Return: Nothing
+ */
+void free_grid(int **grid, int height)
+{
+int i;
+for (i = 0; i < height; i++)
+{
+free(grid[i]);
+}
+free(grid);
+}
